c2a02e77e2818cb3e8edfe14f4e84e26db6e79d7
crisimonc <cristinasimoncini@ESUK-CRISTINA-01.local>
2022-04-19 15:22:03 +0300

    build_grid, mirror_row

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,13 +3,34 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3) # Enum.chunk(hex, 3)
+    |> Enum.map(&mirror_row/1)
+  end
 
+  def mirror_row(row) do
+    # input: [145, 46, 200]
+    [first, second | _tail] = row
+    # output: [145, 46, 200, 46, 145]
+    row ++ [second, first]
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
+
+  # pick_color equivalent in JS:
+  # pick_color: function(image) {
+  #   image.color = {
+  #     r: image.hex[0],
+  #     g: image.hex[1],
+  #     b: image.hex[2]
+  #   };
+  # }
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
